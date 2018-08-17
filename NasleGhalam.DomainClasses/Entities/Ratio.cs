6f1f5eeb4d83db09b71a72a6849ace2ea2e06927
hosein shary <mhshariatmadari@hotmail.com>
2018-08-17 19:20:15 +0430

    db chages ratio and question. and some fixes in educationgroup.

--- NasleGhalam.DomainClasses/Entities/Ratio.cs
+++ NasleGhalam.DomainClasses/Entities/Ratio.cs
@@ -7,7 +7,7 @@
         }
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        
 
         public byte Rate { get; set; }
 
--- NasleGhalam.ViewModels/EducationGroup/EducationGroupWithSubGroupsViewModel.cs
+++ NasleGhalam.ViewModels/EducationGroup/EducationGroupWithSubGroupsViewModel.cs
@@ -6,7 +6,10 @@ namespace NasleGhalam.ViewModels.EducationGroup
 {
     public class EducationGroupWithSubGroupsViewModel
     {
-        public EducationGroupIsCheckedViewModel EducationGroup { get; set; }
+        public int EducationGroupId { get; set; }
+        public string Name { get; set; }
+        public bool IsChecked { get; set; }
+
         public List<EducationSubGroupLessonViewModel> SubGroups { get; set; }
     }
 }

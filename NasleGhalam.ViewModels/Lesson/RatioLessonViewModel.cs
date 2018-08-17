655f6af522630798acc3d428e6338b79c8d38b5e
hosein shary <mhshariatmadari@hotmail.com>
2018-08-17 20:07:38 +0430

    fixed create

--- NasleGhalam.ViewModels/Lesson/RatioLessonViewModel.cs
+++ NasleGhalam.ViewModels/Lesson/RatioLessonViewModel.cs
@@ -14,7 +14,7 @@ namespace NasleGhalam.ViewModels.Lesson
         [Display(Name = "ضریب")]
         [Required(ErrorMessageResourceType = typeof(ErrorResources), ErrorMessageResourceName = "Required")]
         [Range(0, byte.MaxValue, ErrorMessageResourceType = typeof(ErrorResources), ErrorMessageResourceName = "Range")]
-        public byte Rate { get; set; }
+        public byte Ratio { get; set; }
 
         [Display(Name = "زیر گروه درسی")]
         [RequiredDdlValidator(invalidValue: "0", ErrorMessageResourceType = typeof(NasleGhalam.ViewModels.ErrorResources), ErrorMessageResourceName = "RequiredDll")]

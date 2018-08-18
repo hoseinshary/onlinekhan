c4de8910814e6a19fc9dee36ec194b6ad4fd7fbf
mostafa <2280849399>
2018-08-18 18:30:14 +0430

    lesson, add ismain

--- NasleGhalam.ClientApp/src/viewModels/lessonViewModel.js
+++ NasleGhalam.ClientApp/src/viewModels/lessonViewModel.js
@@ -1,14 +1,14 @@
-import {
-  required,
-  displayName,
-  maxLength
-} from 'plugins/vuelidate';
+import { required, displayName, maxLength } from 'plugins/vuelidate';
 
 export default {
   instanceObj: {
     Name: {
       displayName: displayName('نام'),
       maxLength: maxLength(50),
+      required
+    },
+    IsMain: {
+      displayName: displayName('اختصاصی'),
       required
     }
   }
--- NasleGhalam.ClientApp/src/views/lesson/edit.vue
+++ NasleGhalam.ClientApp/src/views/lesson/edit.vue
@@ -6,7 +6,53 @@
                  @close="toggleModalEditStore(false)">
 
     <my-input :model="$v.instanceObj.Name"
-              class="col-md-12" />
+              class="col-md-6" />
+    <my-field class="col-md-6"
+              :model="$v.instanceObj.IsMain">
+      <template slot-scope="data">
+        <q-radio v-model="data.obj.$model"
+                 val="false"
+                 label="خیر" />
+        <q-radio v-model="data.obj.$model"
+                 val="true"
+                 label="بلی" />
+      </template>
+    </my-field>
+    <fieldset class="col-12">
+      <legend>گروههای آموزشی</legend>
+      <div class="row"
+           v-if="eduGroupAndEduSubGroupLst">
+
+        <div class="col-sm-2"
+             v-for="group in eduGroupAndEduSubGroupLst"
+             :key="group.Name">
+          <q-checkbox v-model="group.IsChecked"
+                      :label="group.Name" />
+        </div>
+      </div>
+    </fieldset>
+
+    <div class="col-12"
+         v-for="group in eduGroupAndEduSubGroupLst.filter(x => x.IsChecked)"
+         :key="group.Id">
+      <q-slide-transition>
+
+        <fieldset class="col-12">
+          <legend>گروههای آموزشی</legend>
+          <div class="inline col-sm-4"
+               style="padding-top:5px;"
+               v-for="subGroup in group.SubGroups"
+               :key="subGroup.EducationSubGroupId">
+            <div style="margin-top:20px;">{{subGroup.Name}}:</div>
+            <q-input type="number"
+                     style="margin-right:10px; width:20%;"
+                     v-model="subGroup.Ratio"
+                     float-label='ضریب'></q-input>
+          </div>
+        </fieldset>
+      </q-slide-transition>
+
+    </div>
 
   </my-modal-edit>
 </template>

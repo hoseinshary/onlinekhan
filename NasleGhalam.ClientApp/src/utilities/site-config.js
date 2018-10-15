55b975d470b11d4f602f8a68f87a1895710bcf43
mostafa <2280849399>
2018-10-15 12:01:25 +0330

    fix grade level ddl

--- NasleGhalam.ClientApp/src/utilities/site-config.js
+++ NasleGhalam.ClientApp/src/utilities/site-config.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://localhost:63839';
+const API_URL = 'http://hoseinshary.ir';
+// const API_URL = 'http://localhost:63839';
 // const API_URL = 'http://hoseinshary.ir';
 const GRADE_URL = '/api/grade';
 const GRADE_LEVEL_URL = '/api/gradeLevel';

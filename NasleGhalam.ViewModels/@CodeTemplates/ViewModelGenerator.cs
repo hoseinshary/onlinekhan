73c8af794d43187450264b23629cb6c8d8b8ca68
hosein <hosein@HOSEIN-PC>
2019-06-27 12:09:36 +0430

    .....

--- NasleGhalam.ViewModels/@CodeTemplates/ViewModelGenerator.cs
+++ NasleGhalam.ViewModels/@CodeTemplates/ViewModelGenerator.cs
@@ -1,15 +1,51 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace NasleGhalam.ViewModels.LessonDepartment
+namespace NasleGhalam.ViewModels.Assay
 {
-	public class LessonDepartmentViewModel
+	public class AssayViewModel
 	{
 		[Display(Name = "")]
 		public int Id { get; set; }
 
 
 		[Display(Name = "")]
-		public string Name { get; set; }
+		public string Title { get; set; }
+
+
+		[Display(Name = "")]
+		public int Time { get; set; }
+
+
+		[Display(Name = "")]
+		public int LookupId_Importance { get; set; }
+
+
+		[Display(Name = "")]
+		public int LookupId_Type { get; set; }
+
+
+		[Display(Name = "")]
+		public int LookupId_QuestionType { get; set; }
+
+
+		[Display(Name = "")]
+		public bool IsPublic { get; set; }
+
+
+		[Display(Name = "")]
+		public bool IsOnline { get; set; }
+
+
+		[Display(Name = "")]
+		public int UserId { get; set; }
+
+
+		[Display(Name = "")]
+		public DateTime DateTimeCreate { get; set; }
+
+
+		[Display(Name = "")]
+		public string File { get; set; }
 
 
 	}
---
+++ NasleGhalam.ViewModels/Assey/AssayGetQuestionsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.AccessControl;
+
+namespace NasleGhalam.ViewModels.Assey
+{
+    public class AssayGetQuestionsViewModel
+    {
+        public  Type { get; set; }
+
+
+    }
+}

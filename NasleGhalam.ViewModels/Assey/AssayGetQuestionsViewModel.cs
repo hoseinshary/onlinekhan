4de9f0c0f4464c73c942727ba3113fb7f1195ea8
hosein shary <mhshariatmadari@hotmail.com>
2019-07-09 13:22:58 +0430

    merge fix 2

--- NasleGhalam.ViewModels/Assey/AssayGetQuestionsViewModel.cs
+++ NasleGhalam.ViewModels/Assey/AssayGetQuestionsViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Security.AccessControl;
+using NasleGhalam.ViewModels.Lesson;
 
 namespace NasleGhalam.ViewModels.Assey
 {
     public class AssayGetQuestionsViewModel
     {
-        public  Type { get; set; }
+        public IEnumerable<LessonViewModel> Lessons { get; set; }
+
+
 
 
     }

0695f6eb0f975def446f6f5eb05997ce23e48bd2
alirezaetemadi <alirezaetemadi12@live.com>
2019-07-14 18:50:55 +0430

    login style

--- NasleGhalam.ClientApp/src/views/user/login.vue
+++ NasleGhalam.ClientApp/src/views/user/login.vue
@@ -1,7 +1,69 @@
 <template>
   <q-layout view="lHh Lpr lFf" class="login-layout row items-center">
     <q-page-container class="col-12">
-      <div class="row justify-center">
+      <section class="row">
+        <div class="col-md-7 main-pic"></div>
+        <div class="col-md-5 main-input q-pa-lg">
+          <section class="row justify-start content-center">
+            <div class="logo"></div>
+            <div class="col-xl-8 col-lg-9 col-md-10 col-sm-12 q-py-lg">
+              <q-input
+                v-model="loginUser.Username1"
+                placeholder="نام کاربری"
+                color="white"
+                :before="[
+                  {
+                    icon: 'person',
+                  }
+                ]"
+              />
+            </div>
+            <div class="col-xl-8 col-lg-9 col-md-10 col-sm-12 q-pb-lg">
+              <q-input
+                v-model="loginUser.Password1"
+                type="password"
+                placeholder="رمز عبور"
+                color="white"
+                :before="[
+                  {
+                    icon: 'lock',
+                  }
+                ]"
+              />
+            </div>
+            <!-- <base-input
+              :model="$v.loginUser.Username1"
+              class="text-bold q-title col-xl-8 col-lg-9 col-md-10 col-sm-12"
+              :before="[
+                {
+                  icon: 'person',
+                }
+              ]"
+            />
+            <base-input
+              :model="$v.loginUser.Password1"
+              type="password"
+              class="text-bold q-title col-xl-8 col-lg-9 col-md-10 col-sm-12"
+              @keyup.enter="login"
+              :before="[
+                {
+                  icon: 'lock',
+                }
+              ]"
+            />-->
+            <div class="col-12">
+              <button class="q-mr-sm" @click="login">
+                ورود
+                <q-icon name="keyboard_backspace"></q-icon>
+              </button>
+            </div>
+          </section>
+        </div>
+        <div class="col-12 footer-pic">
+          <span></span>
+        </div>
+      </section>
+      <!-- <div class="row justify-center">
         <q-card class="col-lg-4 col-md-6 col-sm-8">
           <q-card-title class="text-center text-bold text-white shadow-10 card-header">آنلاین خوان</q-card-title>
           <q-card-separator/>
@@ -21,7 +83,7 @@
             </div>
           </q-card-main>
         </q-card>
-      </div>
+      </div>-->
     </q-page-container>
   </q-layout>
 </template>
@@ -51,6 +113,78 @@ export default class UserLoginVue extends Vue {
 
 <style>
 .login-layout {
+  background-color: #034da2;
+}
+
+.login-layout .main-input {
+  height: 80vh;
+}
+.login-layout .main-input .logo {
+  background-image: url("../../assets/img/logo2.png");
+  background-repeat: no-repeat;
+  height: 177px;
+  width: 477px;
+}
+
+.login-layout .main-input > section {
+  height: 100%;
+}
+
+.login-layout .main-input .q-input {
+  background-color: white;
+  height: 80px;
+  border-radius: 20px;
+  padding-bottom: 27px;
+}
+
+.login-layout .main-input .q-input::after,
+.login-layout .main-input .q-input::before {
+  content: none;
+}
+
+.login-layout .main-input input {
+  height: 30px;
+  font-weight: bold;
+  font-size: 25px;
+}
+
+.login-layout .main-input button {
+  height: 80px;
+  width: 170px;
+  border-radius: 20px;
+  border: none;
+  font-weight: bold;
+  font-size: 25px;
+  background-color: #ee6c1e;
+  color: #fffbf7;
+}
+
+.login-layout .main-pic {
+  background-image: url("../../assets/img/login_main.png");
+  background-repeat: no-repeat;
+  /* background-position: center; */
+  /* background-size: cover; */
+  height: 731px;
+  width: 732px;
+}
+
+.login-layout .footer-pic {
+  background-color: #0072bb;
+  height: 20vh;
+  position: relative;
+}
+
+.login-layout .footer-pic span {
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  right: calc(50% - 50px);
+  border-radius: 50px;
+  top: -45px;
+  border: none;
+  background-color: #0072bb;
+}
+/* .login-layout {
   background-image: url("../../assets/img/bg.jpg");
   background-position: center;
   background-repeat: no-repeat;
@@ -69,5 +203,5 @@ export default class UserLoginVue extends Vue {
 
 .login-layout .q-card .q-card-main {
   background-color: white;
-}
+} */
 </style>

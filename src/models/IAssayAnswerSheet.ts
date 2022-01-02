9aa745d7a9218886387d92b3945322cd5cd4be4f
hoseinshary <35192770+hoseinshary@users.noreply.github.com>
2022-01-02 18:01:47 +0330

    initials

---
+++ src/models/IAssayAnswerSheet.ts
@@ -0,0 +1,56 @@
+import IAssayCrete from "./IAssay";
+import utilities from "src/utilities";
+import IUser, { DefaultUser } from "./IUser";
+import { Tashih } from "src/utilities/enumeration";
+
+
+export default interface IAssayAnswerSheet {
+  Id: number;
+  AssayId: number;
+  // Assay?: IAssayCrete;
+  UserId: number;
+  User?: IUser;
+  AssayVarient : number;
+  AssayTime: string;
+  DateTime: string;
+  AnswerTimes:Array<number>;
+  Answers:Array<string>;
+  MaybeList:Array<boolean>;
+  AfterList:Array<boolean>;
+  CantList:Array<boolean>;
+
+}
+
+export const DefaultAssayAnswerSheet: IAssayAnswerSheet = {
+  Id: 0,
+  AssayId:0  ,
+  UserId:0,
+  AssayVarient:0,
+  AssayTime:"",
+  AnswerTimes:[],
+  Answers:[],
+  MaybeList:[],
+  AfterList:[],
+  CantList:[],
+  DateTime: "",
+  // Assay:utilities.cloneObject(),
+  User:utilities.cloneObject(DefaultUser)
+};
+
+
+export class AssayAnswerSheetCorectExam {
+  
+  Tashih :Tashih;
+  NumberOfQuestion : number;
+  Path : string;
+  AnswerPath : string;
+  /**
+   * constructor
+   */
+  constructor() {
+    this.Tashih = 0,
+    this.NumberOfQuestion = 0,
+    this.Path = "",
+    this.AnswerPath = ""
+  }
+}
---
+++ src/views/assay/resualtAssay.vue
@@ -0,0 +1,98 @@
+<template>
+  <bs-modal
+    title="نتیجه آزمون "
+    position="bottom"
+    
+    :show="assayAnswerSheetStore.openModal.resualt"
+    @close="assayAnswerSheetStore.OPEN_MODAL_RESUALT(false)"
+  >
+
+
+    <template slot="header">
+      <q-toolbar slot="header" color="warning" text-color>
+        <q-toolbar-title>نتیجه آزمون </q-toolbar-title>
+        <q-btn dense icon="close" @click="assayAnswerSheetStore.OPEN_MODAL_RESUALT(false)" />
+      </q-toolbar>
+    </template>
+
+    <slot>
+    <section class="col-md-10  " >
+    <div class="col-md-12 row ">
+     
+     <div class="text-green">
+       تعداد سوالات درست : {{2}}
+     </div>
+     <div class="text-red">
+       تعداد سوالات غلط : {{3}}
+     </div>
+     <div class="text-orange">
+       تعداد سوالات نزده : {{2}}
+     </div>
+     
+    </div>
+  </section>
+    </slot>
+
+    <template slot="footer">
+      <base-btn-save-back @click="assayAnswerSheetStore.OPEN_MODAL_RESUALT(false)"></base-btn-save-back>
+      <base-btn-back @click="assayAnswerSheetStore.OPEN_MODAL_RESUALT(false)"></base-btn-back>
+    </template>
+  </bs-modal>
+
+
+</template>
+
+
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import { vxm } from "src/store";
+import util from "src/utilities";
+
+
+
+
+@Component({
+  components: {
+      
+      
+  }
+})
+export default class AssayAnswerSheetVue extends Vue {
+  //#region ### data ###
+  assayStore = vxm.assayStore;
+  assay = vxm.assayStore.assayCreate;
+  assayAnswerSheetStore = vxm.assayAnswerSheetStore;
+  resualt = vxm.assayAnswerSheetStore.assayAnswerSheetResult;
+  answerSheet = vxm.assayAnswerSheetStore.assayAnswerSheet;
+   
+  
+
+  pageAccess = util.getAccess(this.assayStore.modelName);
+  
+  //tempChoice: Array< string> = ["0","1","2","3" ,"4"];
+  //#endregion
+
+  //#region ### computed ###
+ 
+
+  //#endregion
+
+  //#region ### methods ###
+
+
+
+
+  
+  //#endregion
+
+  //#region ### hooks ###
+  created() {
+  
+    this.assayAnswerSheetStore.SET_RESUALT_VUE(this);
+
+  
+  }
+  //#endregion
+}
+</script>
